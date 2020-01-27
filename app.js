744687e6b3b16501c55eca72db82b1ce2b06c447
Emir Karşıyakalı <emirkarsiyakali@gmail.com>
2020-01-27 06:00:45 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+var dataURL = 'data.json';
+
+var App = new Vue({
+    el: '#app',
+    data: {
+        days: [
+            {
+                dayNumber: 1,
+                date: '11th April, 2020',
+                isSelected: true,
+                tracks: [
+                    { trackNumber: 1, isSelected: true },
+                    { trackNumber: 2, isSelected: false },
+                ],
+            }
+        ],
+        speakers: [],
+    },
+    mounted() {
+        axios.get(
+           dataURL
+        ).then(
+            response => {
+                this.speakers = response.data;
+            }
+        );
+    },
+    methods: {
+        getSchedule: function (day, track) {
+            const schedules = [];
+
+            const speakersWithSessions = this.speakers.filter(x => x.sessions.find(y => y.day === day && y.track === track) !== undefined);
+
+            for (const speakerWithSessions of speakersWithSessions) {
+                for (const session of speakerWithSessions.sessions) {
+                    if (session.day !== day || session.track !== track) {
+                        continue;
+                    }
+
+                    const schedule = {
+                        sessionNumber: session.session,
+                        time: session.time,
+                        language: session.language,
+                        title: session.title,
+                        description: session.description,
+                        speaker: speakerWithSessions.name,
+                        avatar: speakerWithSessions.avatar,
+                    };
+
+                    schedules.push(schedule);
+                }
+            }
+
+            schedules.sort((a, b) => a.sessionNumber - b.sessionNumber);
+
+            return schedules;
+        },
+        getRandomSpeakers: function() {
+            return _.sampleSize(this.speakers, 6);
+        }
+    }
+});
